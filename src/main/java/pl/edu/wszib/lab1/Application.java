e95bb1757972cfa3d677f2fd50b36f6dd90c275b
Kesshi00 <kesshiaid00@gmail.com>
2022-10-01 15:29:13 +0200

    Nowa rzecz

--- src/main/java/pl/edu/wszib/lab1/Application.java
+++ src/main/java/pl/edu/wszib/lab1/Application.java
@@ -2,10 +2,7 @@ package pl.edu.wszib.lab1;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        new MyFirstClass().run();
     }
 }
+
---
+++ src/main/java/pl/edu/wszib/lab1/MyFirstClass.java
@@ -0,0 +1,10 @@
+package pl.edu.wszib.lab1;
+
+public class MyFirstClass {
+
+    private String firstField;
+
+    public void run(){
+        System.out.println("firstField = " + firstField);
+    }
+}
