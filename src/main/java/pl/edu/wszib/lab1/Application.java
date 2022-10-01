499e1204010596bc9e1c819d0756ce5c01765227
Kesshi00 <kesshiaid00@gmail.com>
2022-10-01 16:13:03 +0200

    New changes

--- src/main/java/pl/edu/wszib/lab1/Application.java
+++ src/main/java/pl/edu/wszib/lab1/Application.java
@@ -1,10 +1,15 @@
 package pl.edu.wszib.lab1;
 
+import pl.edu.wszib.lab1.mypackage.MySecondClass;
+
 public class Application {
     public static void main(String[] args) {
         MyFirstClass myFirstClass = new MyFirstClass();
         myFirstClass.firstField2 = "Test";
         myFirstClass.run();
+
+        MySecondClass mySecondClass = new MySecondClass();
+        mySecondClass.run();
 
 
 
