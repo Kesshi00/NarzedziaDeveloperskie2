4671d4ffcb2e33a73ef89688e2f9a59e4df7d8d7
Kesshi00 <kesshiaid00@gmail.com>
2022-10-01 15:24:04 +0200

    Teraz powinno byc git ;>

--- src/main/java/pl/edu/wszib/lab1/Application.java
+++ src/main/java/pl/edu/wszib/lab1/Application.java
@@ -3,5 +3,9 @@ package pl.edu.wszib.lab1;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
