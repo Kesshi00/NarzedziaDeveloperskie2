8810bd55b85f88f891e5cd56de7e60e1bfc91525
Kesshi00 <kesshiaid00@gmail.com>
2022-10-01 15:34:48 +0200

    Nowa rzecz

--- src/main/java/pl/edu/wszib/lab1/Application.java
+++ src/main/java/pl/edu/wszib/lab1/Application.java
@@ -2,7 +2,8 @@ package pl.edu.wszib.lab1;
 
 public class Application {
     public static void main(String[] args) {
-        new MyFirstClass().run();
+        MyFirstClass myFirstClass = new MyFirstClass();
+        myFirstClass.run();
     }
 }
 
